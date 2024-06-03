a906789e3fc358fcddcda776c2329bd6e41d8ca1
Мульганов Алексей Олегович <amulganov@phoenixit.ru>
2024-06-03 15:06:14 +0300

    add minimum viable product

---
+++ src/main/java/ArCitBot/ArBotLogic.java
@@ -0,0 +1,71 @@
+package ArCitBot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ArBotLogic extends TelegramLongPollingBot {
+
+    final public String BOT_NAME = "CitizenBot";
+    final public String BOT_TOKEN = "6826020090:AAHq6m_qhZ87SV42sBC8wYoXQmtMJk0XcwM";
+
+    ArBotStorage storage;
+
+    ArBotLogic() {
+        storage = new ArBotStorage();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    public String parseMessage(String textMsg) {
+        String response;
+
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
+        if(textMsg.equals("/start"))
+            response = "Здравствуйте! Вам кого?";
+        else if(textMsg.equals("/male"))
+            response = storage.maleAnswer();
+        else if(textMsg.equals("/female"))
+            response = storage.femaleAnswer();
+        else
+            response = "Я не умею читать! Жмите кнопку \uD83D\uDC47";
+
+        return response;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        try{
+            if(update.hasMessage() && update.getMessage().hasText())
+            {
+                //Извлекаем из объекта сообщение пользователя
+                Message inMess = update.getMessage();
+                //Достаем из inMess id чата пользователя
+                String chatId = inMess.getChatId().toString();
+                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+                String response = parseMessage(inMess.getText());
+                //Создаем объект класса SendMessage - наш будущий ответ пользователю
+                SendMessage outMess = new SendMessage();
+
+                //Добавляем в наше сообщение id чата а также наш ответ
+                outMess.setChatId(chatId);
+                outMess.setText(response);
+
+                //Отправка в чат
+                execute(outMess);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
