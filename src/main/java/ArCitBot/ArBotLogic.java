632d3378107af46a7058e138f3be08cdfd609664
Алексей Соловьёвъ <dzhachinto@inbox.ru>
2024-06-06 15:20:37 +0300

    buttons added

--- src/main/java/ArCitBot/ArBotLogic.java
+++ src/main/java/ArCitBot/ArBotLogic.java
@@ -4,7 +4,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
 
 public class ArBotLogic extends TelegramLongPollingBot {
 
@@ -15,6 +20,7 @@ public class ArBotLogic extends TelegramLongPollingBot {
 
     ArBotLogic() {
         storage = new ArBotStorage();
+        initKeyboard();
     }
 
     @Override
@@ -33,15 +39,38 @@ public class ArBotLogic extends TelegramLongPollingBot {
         //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
         if(textMsg.equals("/start"))
             response = "Здравствуйте! Вам кого?";
-        else if(textMsg.equals("/male"))
+        else if(textMsg.equals("/male") || textMsg.equals("Гражданина \uD83D\uDC68\u200D\uD83E\uDDB0"))
             response = storage.maleAnswer();
-        else if(textMsg.equals("/female"))
+        else if(textMsg.equals("/female") || textMsg.equals("Гражданку \uD83D\uDC69\uD83C\uDFFB\u200D\uD83E\uDDB0"))
             response = storage.femaleAnswer();
         else
             response = "Я не умею читать! Жмите кнопку \uD83D\uDC47";
 
         return response;
     }
+
+
+    ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+    void initKeyboard()
+    {
+        //Создаем объект будущей клавиатуры и выставляем нужные настройки
+        replyKeyboardMarkup.setResizeKeyboard(true); //подгоняем размер
+        replyKeyboardMarkup.setOneTimeKeyboard(false); //скрываем после использования
+
+        //Создаем список с рядами кнопок
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        //Создаем один ряд кнопок и добавляем его в список
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRows.add(keyboardRow);
+        //Добавляем одну кнопку с текстом "Просвяти" наш ряд
+        keyboardRow.add(new KeyboardButton("Гражданина \uD83D\uDC68\u200D\uD83E\uDDB0"));
+        keyboardRow.add(new KeyboardButton("Гражданку \uD83D\uDC69\uD83C\uDFFB\u200D\uD83E\uDDB0"));
+        //добавляем лист с одним рядом кнопок в главный объект
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+
+    ReplyKeyboardMarkup kbrd = new ReplyKeyboardMarkup();
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -60,6 +89,7 @@ public class ArBotLogic extends TelegramLongPollingBot {
                 //Добавляем в наше сообщение id чата а также наш ответ
                 outMess.setChatId(chatId);
                 outMess.setText(response);
+                outMess.setReplyMarkup(replyKeyboardMarkup);
 
                 //Отправка в чат
                 execute(outMess);
